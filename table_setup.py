7156d643992ea8d5c62d15afc3a0e44335700d25
Lucas Chung <lucashlc82@gmail.com>
2024-01-24 22:36:49 -0500

    created script to setup database for player urls

--- table_setup.py
+++ table_setup.py
@@ -1,21 +1,25 @@
-from pandas import pd
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pandas as pd
 
-#connect to mongodb
-client = MongoClient('mongodb+srv://lucashlc:<password>@playerurls.8levfvn.mongodb.net/?retryWrites=true&w=majority')
-database = client['web_scrape']
-collection = database['playerURL']
+uri = "mongodb+srv://lucashlc:Yousuckball82@nba.kewvdct.mongodb.net/?retryWrites=true&w=majority"
 
-#Load Excel Data and turn it into a pandas DataFrame object.
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client["players"] #select the players database
+collection = db["URL"] #select the URL collection (table/document)
+
 excel_file_path = 'Database.xlsx'
-excel_file_df = pd.read_excel(excel_file_path, engine='openpyxl')
+df = pd.read_excel(excel_file_path, engine='openpyxl') #convert the excel file into a pandas dataframe object
 
-#Convert the Excel dataframe object into a list of dictionaries
-data_to_append = excel_file_df.to_dict(oreit='records')
+append_data = df.to_dict(orient='records') #convert the pandas dataframe object into a dictionary.
 
-#Insert list of dictionaries into the collection
-collection.insert_many(data_to_append)
-
-#Close MongoDB Connection
-client.close()
-
+collection.insert_many(append_data)  #append dicionary into the collection document/table on MongoDB
